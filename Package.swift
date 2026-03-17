38085da312ebf590b302b0a1eba30cdaa0299ece
Jaehyeon Kim <kim.jaehyeon@nbt.com>
2026-03-17 12:46:02 +0900

    3.6.0 릴리즈

--- Package.swift
+++ Package.swift
@@ -2,33 +2,34 @@
 
 import PackageDescription
 
-let baseURL = "https://github.com/adison-ads"
-let artifactVer = "3.7.0"
-let checksum = "23h1uy45g1iug312y4fg18y4g1"
+let baseURL = "https://github.com/adison-ads/cookie-oven-webtoon"
+let artifactVer = "3.6.0"
+let checksum = "ea301334b8bcc0ebae62b4210472986f0184460b6d84f5eeaade73d2aff993ba"
 
 let package = Package(
     name: "CookieOvenWebtoon",
     products: [
         .library(
             name: "CookieOvenWebtoon",
-            targets: ["CookieOvenWebtoon", "_CookieOvenWebtoonTarget"]
+            targets: ["BinaryWrapper"]
         )
     ],
     dependencies: [
         .package(url: "https://github.com/adison-ads/adison-offerwall-ios-sdk", exact: "4.6.0")
     ],
     targets: [
-        .binaryTarget(
-            name: "CookieOvenWebtoon",
-            url: "\(baseURL)/cookie-oven-webtoon/releases/download/\(artifactVer)/CookieOvenWebtoon.zip",
-            checksum: checksum
-        ),
         .target(
-            name: "_CookieOvenWebtoonTarget",
+            name: "BinaryWrapper",
             dependencies: [
-                .product(name: "AdisonOfferwallSDK", package: "adison-offerwall-ios-sdk")
+                "CookieOvenWebtoon",
+                .product(name: "AdisonOfferwallSDK", package: "adison-offerwall-ios-sdk"),
             ],
             path: "Sources/_Stub"
+        ),
+        .binaryTarget(
+            name: "CookieOvenWebtoon",
+            url: "\(baseURL)/releases/download/\(artifactVer)/CookieOvenWebtoon.zip",
+            checksum: checksum
         ),
     ]
 )
